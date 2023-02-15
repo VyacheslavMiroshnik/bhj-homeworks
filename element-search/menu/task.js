81cc81050fb5c0362baea9118667b89d0d44a896
Vyacheslav <mepa087@gmail.com>
2023-02-16 00:55:32 +0300

    homework2-2

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+let menuLink = Array.from(document.getElementsByClassName("menu__link"));
+menuLink.forEach((element) => {
+  element.onclick = () => {
+    let menuActive = Array.from(document.getElementsByClassName("menu_active"));
+    if (menuActive.length) {
+      menuActive.forEach((element) => element.classList.remove("menu_active"));
+    }
+    if (element.closest(".menu__item").querySelector(".menu_sub")) {
+      element
+        .closest(".menu__item")
+        .getElementsByClassName("menu_sub")[0]
+        .classList.add("menu_active");
+      return false;
+    }
+  };
+});
