a01b213fcafdf21ef7a5642287cc96c8ebdab3e5
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-02-15 21:01:20 +0300

    homework1-3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -7,26 +7,22 @@ for (let i = 0; i < listHoleElements.length; i++) {
   listHoleElements[i].onclick = function () {
     if (listHoleElements[i].className.includes("hole_has-mole")) {
       countWin++;
-      
     } else {
       countLost++;
-      
     }
     if (countWin === 10) {
-        
       playing = false;
       window.alert("Вы победили");
       countWin = 0;
       countLost = 0;
     }
     if (countLoose === 5) {
-        playing = false;
-        countLost = 0;
-        countWin = 0;
+      playing = false;
+      countLost = 0;
+      countWin = 0;
       window.alert("Вы проиграли");
     }
     elementDead.textContent = countWin;
     elementLost.textContent = countLost;
-    
   };
 }
