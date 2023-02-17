536f668a7596b1d54d1e89dfa436999806137223
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-02-17 13:28:07 +0300

    fix menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,14 +2,23 @@ let menuLink = Array.from(document.getElementsByClassName("menu__link"));
 menuLink.forEach((element) => {
   element.onclick = () => {
     let menuActive = Array.from(document.getElementsByClassName("menu_active"));
-    if (menuActive.length) {
-      menuActive.forEach((element) => element.classList.remove("menu_active"));
-      return;
+    let isActive = element
+      .closest(".menu__item")
+      .querySelector(".menu_sub")
+      .className.includes("menu_active");
+    if (menuActive.length && isActive) {
+      menuActive.forEach((element) => {
+        element.classList.remove("menu_active");
+      });
+      return false;
     }
     if (element.closest(".menu__item").querySelector(".menu_sub")) {
+      menuActive.forEach((element) => {
+        element.classList.remove("menu_active");
+      });
       element
         .closest(".menu__item")
-        .getElementsByClassName("menu_sub")[0]
+        .querySelector(".menu_sub")
         .classList.add("menu_active");
       return false;
     }
