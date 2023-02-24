7c4ad6eebce767e2c30995eca8412b9c54a6d438
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-02-24 14:51:38 +0300

    homework 8-1

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,5 +1,13 @@
 const textArea = document.getElementById("editor");
+const clearBtn = document.getElementById("clear");
 textArea.value = localStorage.getItem("text") ? localStorage.text : "";
+
 textArea.addEventListener("input", (e) => {
   localStorage.text = textArea.value;
+});
+
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  textArea.value = "";
+  localStorage.removeItem("text");
 });
